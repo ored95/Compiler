8eef03550b03d34fe57d2eaefc9cba25ef98d2f9
Oreder <stepup.ored@gmail.com>
2019-04-27 11:38:19 +0300

    Init course project with simple hello world

---
+++ Course Project/C-Compiler/C-Compiler/Driver/Program.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace C_Compiler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("C-Compiler");
+            Console.ReadLine();
+        }
+    }
+}
